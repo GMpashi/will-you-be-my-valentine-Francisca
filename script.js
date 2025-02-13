02abea21a29ba736feac8798f6f625b8198d7452
GMpashi <goodnessmpashi1@gmail.com>
2025-02-13 23:26:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yesBtn").addEventListener("click", function () {
+    document.getElementById("message").classList.remove("hidden");
+});
+
+document.getElementById("noBtn").addEventListener("click", function () {
+    alert("That's not an option, Francisca! 😜");
+});
